8f3cdc7cfa9b97ea3695b766bf8ae8b84c0b0157
Shub Raj Lama <hello@shubraj.com>
2024-10-27 23:09:39 +0545

    email verification added

--- backend/shubrajcom/settings.py
+++ backend/shubrajcom/settings.py
@@ -119,6 +119,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://app.shubraj.com',"http://localhost:8000","http://localhost:8090"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
