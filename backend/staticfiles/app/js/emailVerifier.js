3f4293051890497bcf8fb4bb8ccff3d67bc278f8
Shub Raj Lama <hello@shubraj.com>
2024-10-27 22:30:42 +0545

    email verification added

--- backend/staticfiles/app/js/emailVerifier.js
+++ backend/staticfiles/app/js/emailVerifier.js
@@ -1,3 +1,4 @@
+
 const emailInput = document.getElementById('emailInput');
 const statusOutput = document.getElementById('emailOutput');
 const verifyBtn = document.getElementById('verifyBtn');
@@ -5,5 +6,9 @@ const emailForm = document.getElementById('emailForm');
 
 // Enable verify button only if email format is valid
 emailInput.addEventListener('input', () => {
-    verifyBtn.disabled = !isValidEmail(emailInput.value.trim());
+    if (emailInput.value.trim()){
+        verifyBtn.disabled = false;
+    } else {
+        verifyBtn.disabled = true;
+    }
 });
