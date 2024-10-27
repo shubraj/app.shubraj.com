e8e02b8bfdef9e3f9744530edffd96d2d1a1e3ba
Shub Raj Lama <hello@shubraj.com>
2024-10-27 22:34:56 +0545

    email verification added

--- backend/shubrajcom/urls.py
+++ backend/shubrajcom/urls.py
@@ -32,5 +32,4 @@ urlpatterns = [
     path("robots.txt",TemplateView.as_view(template_name="robots.txt",content_type='text/plain'),name="robots.txt"),
     path("", include('app.urls',namespace='app')),
 
-] 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
